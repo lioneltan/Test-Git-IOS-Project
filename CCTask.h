c4ec8475b7d3985c48592dfb9841bdd2a9af5e21
Lionel Tan Jun Long <lionel@Lionels-MacBook-Pro.local>
2017-06-15 15:45:42 +0800

    added a test class

---
+++ CCTask.h
@@ -0,0 +1,13 @@
+//
+//  CCTask.h
+//  testDirectory
+//
+//  Created by Lionel Tan Jun Long on 15/6/17.
+//  Copyright © 2017 Lionel Tan Jun Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTask : NSObject
+
+@end
---
+++ CCTask.m
@@ -0,0 +1,13 @@
+//
+//  CCTask.m
+//  testDirectory
+//
+//  Created by Lionel Tan Jun Long on 15/6/17.
+//  Copyright © 2017 Lionel Tan Jun Long. All rights reserved.
+//
+
+#import "CCTask.h"
+
+@implementation CCTask
+
+@end
